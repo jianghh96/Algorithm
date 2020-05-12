641c34f66396e3f150de5e0cd839e1659b51db69
Jiang Haihong <1335787820@qq.com>
2020-05-12 18:06:33 +0800

    反中序遍历！！！

---
+++ 剑指offer/Java/树/面试题54_二叉搜索树的第k大节点.java
@@ -0,0 +1,29 @@
+// 给定一棵二叉搜索树，请找出其中第k大的节点。
+
+// 示例 1:
+
+// 输入: root = [3,1,4,null,2], k = 1
+//    3
+//   / \
+//  1   4
+//   \
+//    2
+// 输出: 4
+
+class Solution {
+    int res, k;
+    public int kthLargest(TreeNode root, int k) {
+        this.k = k;
+        dfs(root);
+        return res;
+    }
+    
+    // 反中序遍历
+    public void dfs(TreeNode root){
+        if(root == null) return;
+        dfs(root.right);
+        if(k == 0) return;
+        if(--k == 0) res = root.val;
+        dfs(root.left);
+    }
+}
