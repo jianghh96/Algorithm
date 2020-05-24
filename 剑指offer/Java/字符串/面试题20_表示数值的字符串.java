864d60222c24dd112a03eb353d32ce10b078ea62
Jiang Haihong <1335787820@qq.com>
2020-05-24 15:28:01 +0800

    数学

---
+++ 剑指offer/Java/字符串/面试题20_表示数值的字符串.java
@@ -0,0 +1,29 @@
+// 请实现一个函数用来判断字符串是否表示数值（包括整数和小数）。例如，字符串"+100"、"5e2"、"-123"、"3.1416"、"0123"都表示数值，但"12e"、"1a3.14"、"1.2.3"、"+-5"、"-1E-16"及"12e+5.4"都不是。
+
+class Solution {
+    public boolean isNumber(String s) {
+        if(s == null || s.length() == 0) return false;
+        boolean sign = false, point = false, has_e = false;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'e' || s.charAt(i) == 'E'){
+                // e只能存在一个且不在最后一位
+                if(has_e) return false;
+                has_e = true;
+                if(i == s.length()-1) return false;
+            }else if(s.charAt(i) == '+' || s.charAt(i) == '-'){
+                // +-可存在，但第二次前面必须是e
+                if(i > 0 && s.charAt(i-1) != 'e' && s.charAt(i-1) != 'E')
+                    return false;
+                point = true;
+            }else if(s.charAt(i) == '.'){
+                // .只能存在一次且不能存在e
+                if(point || has_e) return false;
+                point = true;
+            }else{
+                if(s.charAt(i) < '0' || s.charAt(i) > '9')
+                    return false;
+            }
+        }
+        return true;
+    }
+}
