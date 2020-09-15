da471f425edb2789b2746b1058e31cec21c8dc1f
Jiang Haihong <1335787820@qq.com>
2020-09-15 15:49:40 +0800

    Create 328_奇偶链表.java

---
+++ Java/328_奇偶链表.java
@@ -0,0 +1,26 @@
+// 给定一个单链表，把所有的奇数节点和偶数节点分别排在一起。请注意，这里的奇数节点和偶数节点指的是节点编号的奇偶性，而不是节点的值的奇偶性。
+
+// 请尝试使用原地算法完成。你的算法的空间复杂度应为 O(1)，时间复杂度应为 O(nodes)，nodes 为节点总数。
+
+// 示例 1:
+
+// 输入: 1->2->3->4->5->NULL
+// 输出: 1->3->5->2->4->NULL
+
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null || head.next == null || head.next.next == null)
+            return head;
+        ListNode pre = head;
+        ListNode cur = head.next;
+        ListNode node = head.next;
+        while(cur != null && cur.next != null){
+            pre.next = pre.next.next;
+            cur.next = cur.next.next;
+            pre = pre.next;
+            cur = cur.next;
+        }
+        pre.next = node;
+        return head;
+    }
+}
