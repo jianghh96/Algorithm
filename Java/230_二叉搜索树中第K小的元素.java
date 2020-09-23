ddf4ad5544d91c97ca18d67f00432d6bd8ee7b99
Jiang Haihong <1335787820@qq.com>
2020-09-23 22:09:30 +0800

    中序遍历

---
+++ Java/230_二叉搜索树中第K小的元素.java
@@ -0,0 +1,33 @@
+// 给定一个二叉搜索树，编写一个函数 kthSmallest 来查找其中第 k 个最小的元素。
+
+// 说明：
+// 你可以假设 k 总是有效的，1 ≤ k ≤ 二叉搜索树元素个数。
+
+// 示例 :
+
+// 输入: root = [3,1,4,null,2], k = 1
+//    3
+//   / \
+//  1   4
+//   \
+//    2
+// 输出: 1
+
+// 二叉查找树中序遍历有序
+
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while(cur != null || !stack.isEmpty()){
+            while(cur != null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            if(--k == 0) break;
+            cur = cur.right;
+        }
+        return cur.val;
+    }
+}
