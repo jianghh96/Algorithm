8cc02fd01d8bc4d6ecaa3c6b06b2c87f4ba29b72
Jiang Haihong <1335787820@qq.com>
2020-09-27 15:26:36 +0800

    中序遍历

--- Java/98_验证二叉搜索树.java
+++ Java/98_验证二叉搜索树.java
@@ -18,6 +18,20 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+class Solution {
+    long pre = Long.MIN_VALUE;
+    public boolean isValidBST(TreeNode root) {
+        if(root == null) return true;
+        if(!isValidBST(root.left))
+            return false;
+        if(root.val <= pre)
+            return false;
+        pre = root.val;
+        return isValidBST(root.right);
+    }
+}
+
 class Solution {
     public boolean isValidBST(TreeNode root) {
         // 中序遍历！
