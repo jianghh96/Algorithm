89bdcb29741427dcbf456917bb98bb85b6135b16
Jiang Haihong <1335787820@qq.com>
2020-10-04 15:06:39 +0800

    数组排列

--- Java/31_下一个排列.java
+++ Java/31_下一个排列.java
@@ -15,33 +15,36 @@
 
 class Solution {
     public void nextPermutation(int[] nums) {
-        // 难点
-        int i = nums.length - 2;
-        while(i >= 0 && nums[i] >= nums[i+1]){
+        //源于离散数学及其应用的算法：（以3 4 5 2 1 为例）
+        //从后往前寻找第一次出现的正序对：（找到 4,5）
+        //之后因为从5 开始都是逆序，所以把他们反转就是正序：3 4 1 2 5
+        //之后4 的位置应该是：在它之后的，比他大的最小值（5）
+        //交换这两个值：得到 3 5 1 2 4
+        // 对于初始即为逆序的序列，将在反转步骤直接完成
+        int len = nums.length;
+        if(len < 2) return;
+        int i = len-1;
+        while(i > 0 && nums[i-1] >= nums[i]){
             i--;
         }
-        if(i >= 0){
-            int j = nums.length - 1;
-            while(j >= 0 && nums[j] <= nums[i]){
-                j--;
-            }
-            swap(nums, i, j);
+        reverse(nums, i, len-1);
+        if(i == 0) return;
+        int j = i-1;
+        while(i < len && nums[j] >= nums[i]){
+            i++;
         }
-        reverse(nums, i+1);
+        swap(nums, i, j);
     }
-    
-    public void reverse(int[] nums, int start){
-        int len = nums.length - 1;
-        while(start < len){
-            swap(nums, start, len);
-            start++;
-            len--;
+    public void swap(int[] nums, int left, int right){
+        int num = nums[left];
+        nums[left] = nums[right];
+        nums[right] = num;
+    }
+    public void reverse(int[] nums, int left, int right){
+        while(left < right){
+            swap(nums, left, right);
+            left++;
+            right--;
         }
-    }
-
-    public void swap(int[] nums, int i, int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
     }
 }
