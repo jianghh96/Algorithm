b8fde4b4ad393acd8ce8c3c3b644991556d9844a
Jiang Haihong <1335787820@qq.com>
2020-10-10 16:21:43 +0800

    hard 栈

--- Java/85_最大矩形.java
+++ Java/85_最大矩形.java
@@ -16,38 +16,31 @@
 // 接84题柱状图最大矩形方法，继续使用栈方法
 
 class Solution {
-
-    // Get the maximum area in a histogram given its heights
-    public int leetcode84(int[] heights) {
-        Stack < Integer > stack = new Stack < > ();
+    public int leetcode84(int[] heights){
+        if(heights == null || heights.length == 0) return 0;
+        Stack<Integer> stack = new Stack<>();
         stack.push(-1);
-        int maxarea = 0;
-        for (int i = 0; i < heights.length; ++i) {
-            while (stack.peek() != -1 && heights[stack.peek()] >= heights[i])
-                maxarea = Math.max(maxarea, heights[stack.pop()] * (i - stack.peek() - 1));
+        int maxArea = 0;
+        for(int i = 0; i < heights.length; i++){
+            while(stack.peek() != -1 && heights[stack.peek()] >= heights[i])
+                maxArea = Math.max(maxArea, heights[stack.pop()] * (i - stack.peek() - 1));
             stack.push(i);
         }
-        while (stack.peek() != -1)
-            maxarea = Math.max(maxarea, heights[stack.pop()] * (heights.length - stack.peek() -1));
-        return maxarea;
+        while(stack.peek() != -1){
+            maxArea = Math.max(maxArea, heights[stack.pop()] * (heights.length - stack.peek() - 1));
+        }
+        return maxArea;
     }
-
     public int maximalRectangle(char[][] matrix) {
-
-        if (matrix.length == 0) return 0;
-        int maxarea = 0;
-        int[] dp = new int[matrix[0].length];
-
-        for(int i = 0; i < matrix.length; i++) {
-            for(int j = 0; j < matrix[0].length; j++) {
-
-                // update the state of this row's histogram using the last row's histogram
-                // by keeping track of the number of consecutive ones
-
-                dp[j] = matrix[i][j] == '1' ? dp[j] + 1 : 0;
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return 0;
+        int[] nums = new int[matrix[0].length];
+        int res = 0;
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                nums[j] = matrix[i][j] == '1' ? nums[j]+1 : 0; 
             }
-            // update maxarea with the maximum area from this row's histogram
-            maxarea = Math.max(maxarea, leetcode84(dp));
-        } return maxarea;
+            res = Math.max(res, leetcode84(nums));
+        }
+        return res;
     }
 }
