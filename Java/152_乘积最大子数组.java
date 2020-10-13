52a1b8f17381e6c63fc5c8270b318c91b8b44d49
Jiang Haihong <1335787820@qq.com>
2020-10-13 14:19:17 +0800

    动态规划

--- Java/152_乘积最大子数组.java
+++ Java/152_乘积最大子数组.java
@@ -26,3 +26,21 @@ class Solution {
         return max;
     }
 }
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        int res = nums[0], max = nums[0], min = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] < 0){
+                int tmp = max;
+                max = min;
+                min = tmp;
+            }
+            max = Math.max(nums[i], max * nums[i]);
+            min = Math.min(nums[i], min * nums[i]);
+            res = Math.max(res, max);
+        }
+        return res;
+    }
+}
