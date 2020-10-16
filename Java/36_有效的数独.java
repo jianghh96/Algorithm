9959b62059ebf6129d605120cfe12021fbedbae1
Jiang Haihong <1335787820@qq.com>
2020-10-16 20:17:27 +0800

    哈希表

---
+++ Java/36_有效的数独.java
@@ -0,0 +1,58 @@
+// 判断一个 9x9 的数独是否有效。只需要根据以下规则，验证已经填入的数字是否有效即可。
+
+// 数字 1-9 在每一行只能出现一次。
+// 数字 1-9 在每一列只能出现一次。
+// 数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。
+
+// 数独部分空格内已填入了数字，空白格用 '.' 表示。
+
+// 示例 :
+
+// 输入:
+// [
+//   ["5","3",".",".","7",".",".",".","."],
+//   ["6",".",".","1","9","5",".",".","."],
+//   [".","9","8",".",".",".",".","6","."],
+//   ["8",".",".",".","6",".",".",".","3"],
+//   ["4",".",".","8",".","3",".",".","1"],
+//   ["7",".",".",".","2",".",".",".","6"],
+//   [".","6",".",".",".",".","2","8","."],
+//   [".",".",".","4","1","9",".",".","5"],
+//   [".",".",".",".","8",".",".","7","9"]
+// ]
+
+// 输出: true
+
+// 说明:
+
+// 一个有效的数独（部分已被填充）不一定是可解的。
+// 只需要根据以上规则，验证已经填入的数字是否有效即可。
+// 给定数独序列只包含数字 1-9 和字符 '.' 。
+// 给定数独永远是 9x9 形式的。
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashMap<Integer, Integer> [] rows = new HashMap[9];
+        HashMap<Integer, Integer> [] cols = new HashMap[9];
+        HashMap<Integer, Integer> [] boxes = new HashMap[9];
+        for(int i = 0; i < 9; i++){
+            rows[i] = new HashMap<Integer, Integer>();
+            cols[i] = new HashMap<Integer, Integer>();
+            boxes[i] = new HashMap<Integer, Integer>();
+        }
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(board[i][j] != '.'){
+                    int num = (int) board[i][j];
+                    rows[i].put(num, rows[i].getOrDefault(num, 0)+1);
+                    cols[i].put(num, cols[i].getOrDefault(num, 0)+1);
+                    int index = (i / 3) * 3 + j / 3;
+                    boxes[index].put(num, boxes[index].getOrDefault(num, 0)+1);
+                    if(rows[i].get(num) > 1 || cols[i].get(num) > 1 || boxes[index].get(num) > 1)
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+}
