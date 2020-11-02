328a2d73eae8ba2d724ac279f3d6d9c69b4c50db
Jiang Haihong <1335787820@qq.com>
2020-11-02 22:20:07 +0800

    哈希表 数组

--- Java/36_有效的数独.java
+++ Java/36_有效的数独.java
@@ -56,3 +56,27 @@ class Solution {
         return true;
     }
 }
+
+// 推荐这种方法
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] cols = new boolean[9][9];
+        boolean[][] boxes = new boolean[9][9];
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(board[i][j] != '.'){
+                    int box = i / 3 * 3 + j / 3;
+                    int num = board[i][j] - '1';
+                    if(rows[i][num] || cols[j][num] || boxes[box][num])
+                        return false;
+                    rows[i][num] = true;
+                    cols[j][num] = true;
+                    boxes[box][num] = true;
+                }
+            }
+        }
+        return true;
+    }
+}
