c1fff8421ef2a4deafe9b2110ce865a715101a9b
Jiang Haihong <1335787820@qq.com>
2020-11-12 21:33:08 +0800

    顺时针遍历+旋转矩阵

--- Java/54_螺旋矩阵.java
+++ Java/54_螺旋矩阵.java
@@ -20,6 +20,8 @@
 // ]
 // 输出: [1,2,3,4,8,12,11,10,9,5,6,7]
 
+// 旋转矩阵
+
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> res = new ArrayList<>();
@@ -40,5 +42,34 @@ class Solution {
             }
         }
         return mtx;
+    }
+}
+
+// 顺时针遍历
+
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return res;
+        int left = 0, right = matrix[0].length-1, top = 0, below = matrix.length-1;
+        while(left <= right && top <= below){
+            for(int i = left; i <= right; i++)
+                res.add(matrix[top][i]);
+            top++;
+            if(top > below) break;
+            for(int i = top; i <= below; i++)
+                res.add(matrix[i][right]);
+            right--;
+            if(right < left) break;
+            for(int i = right; i >= left; i--)
+                res.add(matrix[below][i]);
+            below--;
+            if(below < top) break;
+            for(int i = below; i >= top; i--)
+                res.add(matrix[i][left]);
+            left++;
+            if(left > right) break;
+        }
+        return res;
     }
 }
