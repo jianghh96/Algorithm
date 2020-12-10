49874ff5081387c1cc2cbd50011a3c3b1e173035
Jiang Haihong <1335787820@qq.com>
2020-12-10 17:13:36 +0800

    位运算

--- 剑指offer/Java/二进制/剑指Offer56-II_数组中数字出现的次数II.java
+++ 剑指offer/Java/二进制/剑指Offer56-II_数组中数字出现的次数II.java
@@ -35,3 +35,22 @@ class Solution {
         return ones;
     }
 }
+
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int[] counts = new int[32];
+        for(int num : nums){
+            for(int i = 0; i < 32; i++){
+                counts[i] += num & 1;
+                num >>= 1;
+            }
+        }
+        int res = 0;
+        for(int i = 31; i >= 0; i--){
+            res <<= 1;
+            res |= counts[i] % 3;
+        }
+        return res;
+    }
+}
